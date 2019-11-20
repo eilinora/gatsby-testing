1845fbf1b17dd7419cfa78460c5b9a8bc7e99eb3
Lori Hutchek <Lori.Hutchek@akqa.com>
2019-11-20 17:01:17 -0500

    initial components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: `purple` }}>
+    <Header title="Heyo!" />
+    <nav>
+      <Link to="/about">About page</Link>
+      <Link to="/contact">Contact page</Link>
+    </nav>
+    <p>some subtitle</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+)
